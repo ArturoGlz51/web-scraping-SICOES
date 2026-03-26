95b502f3fed6ba525e7154610c446ed3e3f99e60
Aquizi <joseaquino27042005@gmail.com>
2026-03-25 23:15:50 -0700

    Delete web-scraping-SICOES-main directory

--- web-scraping-SICOES-main/acceso_datos/guardar_json.py
+++
@@ -1,6 +0,0 @@
-import json
-
-def guardar_json(datos, ruta="calificaciones.json"):
-    with open(ruta, "w", encoding="utf-8") as f:
-        json.dump(datos, f, ensure_ascii=False, indent=4)
-    print(f"Datos guardados en {ruta}")
